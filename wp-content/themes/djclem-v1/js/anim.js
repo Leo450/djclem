15a5d0c3ac6a54906467ff9d0b8b940fa1060aae
Leo450 <leo450450@gmail.com>
2018-11-28 18:06:42 +0100

    it's drawing

--- wp-content/themes/djclem-v1/js/anim.js
+++ wp-content/themes/djclem-v1/js/anim.js
@@ -4,8 +4,32 @@
 
 		'use strict';
 
-		let ld = new LetterDrawer(document.getElementById("anim-logo"), {width: 120, height: 120}, 10, "#ffffff");
-		ld.drawCircular(50);
+		let ld = new LetterDrawer({
+			canvas: document.getElementById("anim-logo"),
+			size: {width: 120, height: 120},
+			travel: [
+				{
+					start: 0,
+					end: 50,
+					type: "arc",
+					settings: {
+						radius: 50
+					}
+				},
+				{
+					start: 50,
+					end: 100,
+					type: "arc",
+					settings: {
+						radius: 20
+					}
+				}
+			],
+			duration: 5
+		});
+		setTimeout(() => {
+			ld.draw();
+		}, 1000);
 
 	});
 
--- wp-content/themes/djclem-v1/js/lib/LetterDrawer/LetterDrawer.js
+++ wp-content/themes/djclem-v1/js/lib/LetterDrawer/LetterDrawer.js
@@ -1,14 +1,24 @@
 class LetterDrawer
 {
 
-	constructor(canvas, size, strokeWidth, color)
+	constructor(options)
 	{
 
-		this.canvas = canvas;
+		this.drawLoop = this.drawLoop.bind(this);
+
+		this.canvas = options.canvas;
 		this.context = this.canvas.getContext("2d");
-		this.size = size;
-		this.strokeWidth = strokeWidth;
-		this.color = color;
+		this.size = options.size || {width: 100, height: 100};
+		this.center = {
+			x: this.size.width/2,
+			y: this.size.height/2
+		};
+		this.strokeWidth = options.strokeWidth || 10;
+		this.color = options.color || "#ffffff";
+		this.travel = options.travel || null;
+		this.duration = options.duration || null;
+
+		this.startTime = null;
 
 		this.initCanvas();
 		this.initContext();
@@ -17,6 +27,10 @@ class LetterDrawer
 
 	initCanvas()
 	{
+
+		if(!this.size.width || !this.size.height){
+			throw new Error("!");
+		}
 
 		this.canvas.width = this.size.width;
 		this.canvas.height = this.size.height;
@@ -34,26 +48,111 @@ class LetterDrawer
 
 	}
 
-	drawCircular(radius, center, angle, startAngle)
+	clearCanvas()
 	{
 
-		// Default values
-		if(center === undefined){
-			center = {
-				x: this.size.width/2,
-				y: this.size.height/2
+		this.context.clearRect(0, 0, this.size.width, this.size.height);
+
+	}
+
+	draw()
+	{
+
+		if(!this.travel){
+			throw new Error("!");
+		}
+		if(!this.duration){
+			throw new Error("!");
+		}
+
+		this.startTime = Date.now();
+
+		this.drawLoop();
+
+	}
+
+	drawLoop()
+	{
+
+		// step = {start: 20, end: 50}
+		// /-----|---------------/
+		//       21%
+		//     |<-30%->| : {start: 0, end: 30 (end - start)(50 - 20)}
+		//    20%     50%
+		//       |
+		//       1% : (progress - start)(21 - 20)
+
+		const currentTime = Date.now();
+		const elapsedTime = currentTime - this.startTime;
+		const progress = elapsedTime/(this.duration*1000);
+
+		this.clearCanvas();
+
+		let steps = this.getRunningSteps(progress);
+		for(let i = 0; i < steps.length; i++){
+			const step = steps[i];
+			const start = step.start/100;
+			const end = step.end/100;
+			const stepProgress = (progress - start)/(end - start);
+
+			const drawMethodName = "drawStep" + step.type.substr(0, 1).toUpperCase() + step.type.substr(1);
+			if(this[drawMethodName] === undefined){
+				throw new Error("!");
+			}
+			this[drawMethodName](step.settings, stepProgress);
+
+		}
+
+		if(progress >= 1){
+			return;
+		}
+
+		requestAnimationFrame(this.drawLoop);
+
+	}
+
+	getRunningSteps(progress)
+	{
+
+		let steps = [];
+
+		for(let i = 0; i < this.travel.length; i++){
+			const step = this.travel[i];
+			if(step.start/100 < progress){
+				steps.push(step);
 			}
 		}
-		if(angle === undefined){
-			angle = Math.PI*2;
+
+		return steps;
+
+	}
+
+	drawStepArc(settings, progress)
+	{
+
+		const radius = settings.radius;
+		const center = settings.center || this.center;
+		let   angle = settings.angle || Math.PI*2;
+		const startAngle = (settings.startAngle !== undefined && settings.startAngle !== null) ? settings.startAngle : 0;
+
+		if(!radius){
+			throw new Error("!");
 		}
-		if(startAngle === undefined){
-			startAngle = 0;
+		if(isNaN(center.x) || isNaN(center.y)){
+			throw new Error("!");
 		}
 
+		if(progress < 1){
+			angle = angle*progress;
+		}
 
+		this.drawArc(radius, center, angle, startAngle);
 
-		// Draw
+	}
+
+	drawArc(radius, center, angle, startAngle)
+	{
+
 		this.context.beginPath();
 		this.context.arc(center.x, center.y, radius, startAngle, angle + startAngle);
 		this.context.stroke();
